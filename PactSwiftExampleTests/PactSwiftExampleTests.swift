25e55022c9f634fa34b060e1ad2ab85ab0e39cd3
Andrew Spinks <andrewspinks@gmail.com>
2015-02-05 14:04:09 +0900

    Add working test example

--- PactSwiftExampleTests/PactSwiftExampleTests.swift
+++ PactSwiftExampleTests/PactSwiftExampleTests.swift
@@ -1,5 +1,7 @@
 import UIKit
 import XCTest
+import PactSwiftExample
+import PactConsumerSwift
 
 class PactSwiftExampleTests: XCTestCase {
 
@@ -14,15 +16,27 @@ class PactSwiftExampleTests: XCTestCase {
     }
 
     func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
+      var hello = "not Goodbye"
+      var helloProvider = MockService(provider: "Hello Provider", consumer: "Hello Consumer")
+      let expectation = expectationWithDescription("Responds with hello")
+      
+      helloProvider.uponReceiving("a request for hello")
+        .withRequest(PactHTTPMethod.Get, path: "/sayHello")
+        .willRespondWith(200, headers: ["Content-Type": "application/json"], body: [ "reply": "Hello"])
+      
+      //Run the tests
+      helloProvider.run ( { (complete) -> Void in
+        HelloClient(baseUrl: helloProvider.baseUrl).sayHello { (response) in
+          hello = response
+          complete()
         }
+        }, result: { (verification) -> Void in
+          // Important! This ensures all expected HTTP requests were actually made.
+          XCTAssertEqual(verification, PactVerificationResult.Passed)
+          expectation.fulfill()
+      })
+      
+      waitForExpectationsWithTimeout(10) { (error) in }
     }
 
 }
