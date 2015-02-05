2374352c1ed2c4d32ae8c841d526a265697297db
Andrew Spinks <andrewspinks@gmail.com>
2015-02-05 11:40:26 +0900

    Initial commit.

---
+++ PactSwiftExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PactSwiftExample
+//
+//  Created by Andrew Spinks on 5/02/2015.
+//  Copyright (c) 2015 Andrew Spinks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
