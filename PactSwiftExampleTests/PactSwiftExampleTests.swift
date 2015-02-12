14af5e8a099ac4f3bdb04053093445221a69b690
Andrew Spinks <andrewspinks@gmail.com>
2015-02-12 13:20:17 +0900

    Update to simpler syntax

--- PactSwiftExampleTests/PactSwiftExampleTests.swift
+++ PactSwiftExampleTests/PactSwiftExampleTests.swift
@@ -10,7 +10,11 @@ class PactSwiftExampleTests: XCTestCase {
 
   override func setUp() {
     super.setUp()
-    helloProvider = MockService(provider: "Hello Provider", consumer: "Hello Consumer")
+    let expectation = expectationWithDescription("Pacts are verified")
+    helloProvider = MockService(provider: "Hello Provider", consumer: "Hello Consumer", done: { result in
+      XCTAssertEqual(result, PactVerificationResult.Passed)
+      expectation!.fulfill()
+    })
     helloClient = HelloClient(baseUrl: helloProvider!.baseUrl)
   }
 
@@ -20,50 +24,37 @@ class PactSwiftExampleTests: XCTestCase {
 
   func testItSaysHello() {
     var hello = "not Goodbye"
-    var helloProvider = MockService(provider: "Hello Provider", consumer: "Hello Consumer")
-    let expectation = expectationWithDescription("Responds with hello")
-
-    helloProvider.uponReceiving("a request for hello")
+    helloProvider!.uponReceiving("a request for hello")
                   .withRequest(PactHTTPMethod.Get, path: "/sayHello")
                   .willRespondWith(200, headers: ["Content-Type": "application/json"], body: ["reply": "Hello"])
 
     //Run the tests
-    helloProvider.run({ (testComplete) -> Void in
-        HelloClient(baseUrl: helloProvider.baseUrl).sayHello { (response) in
-          XCTAssertEqual(response, "Hello")
-          testComplete()
-        }
+    helloProvider!.run{ (testComplete) -> Void in
+      self.helloClient!.sayHello { (response) in
+        XCTAssertEqual(response, "Hello")
+        testComplete()
       }
-    ).onComplete { result in
-      XCTAssertEqual(result, PactVerificationResult.Passed)
-      expectation.fulfill()
     }
 
     waitForExpectationsWithTimeout(10) { (error) in }
   }
 
   func test404Error() {
-    let expectation = expectationWithDescription("Error occured")
-
     helloProvider!.given("I have no friends")
-      .uponReceiving("a request to unfriend")
-      .withRequest(PactHTTPMethod.Put, path: "/unfriendMe")
-      .willRespondWith(404, body: "No friends")
+                  .uponReceiving("a request to unfriend")
+                  .withRequest(PactHTTPMethod.Put, path: "/unfriendMe")
+                  .willRespondWith(404, body: "No friends")
 
     //Run the tests
-    helloProvider!.run({ (testComplete) -> Void in
-        self.helloClient!.unfriendMe({ (response) in
-            XCTAssertFalse(true)
-            testComplete()
-          }, errorResponse: { (error) in
-            XCTAssertEqual(error, 404)
-            testComplete()
-          }
-        )
-      }
-    ).onComplete { result in
-      XCTAssertEqual(result, PactVerificationResult.Passed)
-      expectation.fulfill()
+    helloProvider!.run{ (testComplete) -> Void in
+      self.helloClient!.unfriendMe({ (response) in
+          XCTAssertFalse(true)
+          testComplete()
+        }, errorResponse: { (error) in
+          XCTAssertEqual(error, 404)
+          testComplete()
+        }
+      )
     }
 
     waitForExpectationsWithTimeout(10) { (error) in }
