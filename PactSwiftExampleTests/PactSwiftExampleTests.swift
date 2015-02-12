16e6d37981c353598c4336eca91fc280213f16ef
Andrew Spinks <andrewspinks@gmail.com>
2015-02-12 11:12:30 +0900

    Update to new test syntax.

--- PactSwiftExampleTests/PactSwiftExampleTests.swift
+++ PactSwiftExampleTests/PactSwiftExampleTests.swift
@@ -28,74 +28,16 @@ class PactSwiftExampleTests: XCTestCase {
                   .willRespondWith(200, headers: ["Content-Type": "application/json"], body: ["reply": "Hello"])
 
     //Run the tests
-    helloProvider.run({
-      (complete) -> Void in
-      HelloClient(baseUrl: helloProvider.baseUrl).sayHello {
-        (response) in
-        XCTAssertEqual(response, "Hello")
-        complete()
+    helloProvider.run({ (testComplete) -> Void in
+        HelloClient(baseUrl: helloProvider.baseUrl).sayHello { (response) in
+          XCTAssertEqual(response, "Hello")
+          testComplete()
+        }
       }
-    }, verification: {
-      (verification) -> Void in
-      // Important! This ensures all expected HTTP requests were actually made.
-      XCTAssertEqual(verification, PactVerificationResult.Passed)
+    ).onComplete { result in
+      XCTAssertEqual(result, PactVerificationResult.Passed)
       expectation.fulfill()
-    })
-
-    waitForExpectationsWithTimeout(10) { (error) in }
-  }
-
-  func testRequestWithQueryParams() {
-    var helloProvider = MockService(provider: "Hello Provider", consumer: "Hello Consumer")
-    let expectation = expectationWithDescription("Responds with friends")
-
-    helloProvider.uponReceiving("a request friends")
-                  .withRequest(PactHTTPMethod.Get, path: "/friends", query: [ "age" : "30", "child" : "Mary" ])
-                  .willRespondWith(200, headers: ["Content-Type": "application/json"], body: [ "friends": ["Sue"] ])
-
-    //Run the tests
-    helloProvider.run({
-      (complete) -> Void in
-      self.helloClient!.findFriendsByAgeAndChild {
-        (response) in
-        XCTAssertEqual(response[0], "Sue")
-        complete()
-      }
-    }, verification: {
-      (verification) -> Void in
-      XCTAssertEqual(verification, PactVerificationResult.Passed)
-      expectation.fulfill()
-    })
-
-    waitForExpectationsWithTimeout(10) { (error) in }
-  }
-
-  func testPutRequest() {
-    let expectation = expectationWithDescription("Unfriends fred")
-
-    helloProvider!.given("I am friends with Fred")
-                  .uponReceiving("a request to unfriend")
-                  .withRequest(PactHTTPMethod.Put, path: "/unfriendMe")
-                  .willRespondWith(200, headers: ["Content-Type": "application/json"], body: [ "reply": "Bye" ])
-
-    //Run the tests
-    helloProvider!.run({
-      (complete) -> Void in
-      self.helloClient!.unfriendMe( {
-        (response) in
-        XCTAssertEqual(response["reply"]!, "Bye")
-        complete()
-      }, errorResponse: {
-        (error) in
-        XCTAssertFalse(true)
-        complete()
-      })
-    }, verification: {
-      (verification) -> Void in
-      // Important! This ensures all expected HTTP requests were actually made.
-      XCTAssertEqual(verification, PactVerificationResult.Passed)
-      expectation.fulfill()
-    })
+    }
 
     waitForExpectationsWithTimeout(10) { (error) in }
   }
@@ -109,23 +51,20 @@ class PactSwiftExampleTests: XCTestCase {
       .willRespondWith(404, body: "No friends")
 
     //Run the tests
-    helloProvider!.run({
-      (complete) -> Void in
-      self.helloClient!.unfriendMe({
-        (response) in
-        XCTAssertFalse(true)
-        complete()
-      }, errorResponse: {
-        (error) in
-        XCTAssertEqual(error, 404)
-        complete()
-      })
-    }, verification: {
-      (verification) -> Void in
-      // Important! This ensures all expected HTTP requests were actually made.
-      XCTAssertEqual(verification, PactVerificationResult.Passed)
+    helloProvider!.run({ (testComplete) -> Void in
+        self.helloClient!.unfriendMe({ (response) in
+            XCTAssertFalse(true)
+            testComplete()
+          }, errorResponse: { (error) in
+            XCTAssertEqual(error, 404)
+            testComplete()
+          }
+        )
+      }
+    ).onComplete { result in
+      XCTAssertEqual(result, PactVerificationResult.Passed)
       expectation.fulfill()
-    })
+    }
 
     waitForExpectationsWithTimeout(10) { (error) in }
   }
